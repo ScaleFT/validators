75467b2a41988b4fd20b31fc3e4edef1ed801515
Brad Morgan <brad@morgabra.com>
2017-05-08 17:17:27 -0700

    Add Port and HostnameAndPort validators.

--- schema_validator.go
+++ schema_validator.go
@@ -1,6 +1,9 @@
 package validators
 
 import (
+	"net"
+	"strconv"
+
 	"golang.org/x/net/idna"
 
 	"github.com/russellhaering/jsonmap"
@@ -26,6 +29,77 @@ func (h *HostNameValidator) Validate(v interface{}) (munged interface{}, err err
 			return munged, jsonmap.NewValidationError("punycode translation exceeds maximum length")
 		}
 		return val, nil
+	}
+	return munged, jsonmap.NewValidationError("invalid type")
+}
+
+// PortValidator implements the jsonmap Validator interface.
+type PortValidator struct{}
+
+// Validate ensures that the port given is valid (empty OR non-zero uint16).
+// 0 ports are legal but will fail this validator.
+// v: the the value to be verified, should be a string
+// returns:
+//   munged: the, possibly modified, value
+//   err: any errors encountered
+func (h *PortValidator) Validate(v interface{}) (munged interface{}, err error) {
+	if val, ok := v.(string); ok {
+		if val == "" {
+			return "", nil
+		}
+
+		p, err := strconv.ParseUint(val, 10, 16)
+		if err != nil {
+			return munged, jsonmap.NewValidationError("invalid port: %s", err.Error())
+		}
+		if p == 0 {
+			return munged, jsonmap.NewValidationError("invalid port: Port cannot be 0")
+		}
+		return strconv.FormatUint(p, 10), nil
+	}
+	return munged, jsonmap.NewValidationError("invalid type")
+}
+
+// HostNameAndPortValidator implements the jsonmap Validator interface.
+type HostNameAndPortValidator struct{}
+
+// Validate ensures that the hostname given is a valid unicode hostname and optional port.
+// v: the the value to be verified, should be a string
+// returns:
+//   munged: the, possibly modified, value
+//   err: any errors encountered
+func (h *HostNameAndPortValidator) Validate(v interface{}) (munged interface{}, err error) {
+	if val, ok := v.(string); ok {
+		hostname, port, err := net.SplitHostPort(val)
+		if err != nil {
+			return munged, jsonmap.NewValidationError("invalid hostname/port: %s", err.Error())
+		}
+
+		// Validate hostname
+		hv := &HostNameValidator{}
+		munged, err := hv.Validate(hostname)
+		if err != nil {
+			return munged, err
+		}
+		if hostname, ok = munged.(string); !ok {
+			return munged, jsonmap.NewValidationError("invalid hostname type")
+		}
+
+		// Validate port
+		pv := &PortValidator{}
+		munged, err = pv.Validate(port)
+		if err != nil {
+			return munged, err
+		}
+		if port, ok = munged.(string); !ok {
+			return munged, jsonmap.NewValidationError("invalid port type")
+		}
+
+		// Omit empty port
+		if port != "" {
+			return net.JoinHostPort(hostname, port), nil
+		}
+		return hostname, nil
 	}
 	return munged, jsonmap.NewValidationError("invalid type")
 }
